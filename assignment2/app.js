a0f0d9943ee2de64c85d9f4f3482dc6e127937ab
caydendevelop <caydendevelop@gmail.com>
2020-10-27 14:01:37 +0800

    modify app.js

---
+++ assignment2/app.js
@@ -0,0 +1,5 @@
+let body = document.getElementsByTagName('body')[0];
+header = document.createElement('header');
+header.id = 'headerID';
+header.className = 'headerCSS';
+body.appendChild('header');
