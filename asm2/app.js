58a06fb3d659089e5c274c31a45c6e0c77e9fa75
caydendevelop <caydendevelop@gmail.com>
2020-10-27 16:00:01 +0800

    modify sth

--- asm2/app.js
+++ asm2/app.js
@@ -1,9 +1,16 @@
 // creating header
 let body = document.getElementsByTagName('body')[0];
 let headerBlock = document.createElement('header');
-headerBlock.id = 'headerBlockID';
+
 headerBlock.className = 'headerBlockCSS';
+headerBlock.id = 'headerBlockID';
+
+let content = "";
+content += '<button style="float:right;">reload</button>';
+content += '<br/><h1>Weather in Hong Kong</h1>';
+headerBlock.innerHTML = content;
 body.appendChild(headerBlock);
+
 
 
 
